8fc40995edc3b9788338cb82bd34ca730dae924e
thiagollg17 <120064737+thiagollg17@users.noreply.github.com>
2022-12-17 09:35:10 -0300

    js

---
+++ funciones.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.round((Math.random()*5)+1);
+var randomNumber2 = Math.round((Math.random()*5)+1);
+
+var elemento = document.querySelectorAll("img");
+console.log(elemento);
+
+elemento[0].setAttribute("src", "images/dice"+randomNumber1+".png");
+elemento[1].setAttribute("src", "images/dice"+randomNumber2+".png");
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
+
+
